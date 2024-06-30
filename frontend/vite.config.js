4c2e79e6df8800767607e15d818d8cc197b07743
rsyahbudin <syahbudin45@gmail.com>
2024-06-30 14:34:04 +0700

    Add vercel.json for routing configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       "@": "/src", // Adjust the path based on your actual project structure
